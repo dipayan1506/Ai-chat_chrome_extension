f25b12dd0a10462f414decab46104fce090c2b9b
Dipayan <debnathdipayan042@gmail.com>
2025-07-16 23:40:21 +0530

    fix:content.tsx issue fixed

---
+++ src/content.tsx
@@ -0,0 +1,16 @@
+import './index.css'
+
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import ContentPage from '@/content/content'
+
+const root = document.createElement('div')
+root.id = '__leetcode_ai_whisper_container'
+document.body.append(root)
+
+createRoot(root).render(
+  <StrictMode>
+    <ContentPage />
+    {/* <>hi</> */}
+  </StrictMode>
+)
